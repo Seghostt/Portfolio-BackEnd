49a56a8c8f25faaf6d8aff6f4a6977b176d00a9b
seghostt <deimos@outlook.es>
2023-03-02 14:25:17 -0300

    cors allow fix

--- src/main/java/com/DatabaseTest/DTtest/WebConfig.java
+++ src/main/java/com/DatabaseTest/DTtest/WebConfig.java
@@ -1,21 +1,16 @@
+package com.DatabaseTest.DTtest;
 
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
- 
 
 @EnableWebMvc
-
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+    @Override   
     public void addCorsMappings(CorsRegistry registry) 
     {
        registry.addMapping("/**");
